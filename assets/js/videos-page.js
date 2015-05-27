6bd0215979a1dd0e12d98b7a15eaf5c5a0d2c90d
Daniel Vianna <dvianna@bunnings.com.au>
2015-05-27 14:32:27 +0800

    Prepend videos: jQuery implementation.

--- assets/js/videos-page.js
+++ assets/js/videos-page.js
@@ -1,4 +1,36 @@
 $(function whenDomIsReady() {
+    $('.the-submit-video-form').submit(function(e) {
+        e.preventDefault();
+
+        var newVideo = {
+            title: $('.the-submit-video-form input[name="title"]').val(),
+            src: $('.the-submit-video-form input[name="src"]').val()
+        };
+
+        $('.the-submit-video-form input').val('');
+        $('.the-submit-video-form button').text('submitting...');
+        $('.the-submit-video-form button').prop('disabled', true);
+
+        var parser = document.createElement('a');
+        parser.href = newVideo.src
+        var youtubeID = parser.search.substring(parser.search.indexOf("=") + 1,
+                                                parser.search.length);
+        newVideo.src = 'https://www.youtube.com/embed/' + youtubeID;
+
+        setTimeout(function(){
+            var newVideoHtml = '<li class="video">' +
+                '   <h2>' + newVideo.title + '</h2>' +
+                '   <iframe width="640" height="390" src="' + newVideo.src +
+                '" frameborder="0" allowfullscreen></iframe>' + '</li>';
+
+            $('.the-list-of-videos').prepend(newVideoHtml);
+            $('.the-submit-video-form button').text('Submit Video');
+            $('.the-submit-video-form button').prop('disabled', false);
+        }, 750);
+    });
+
+
+//$(function whenDomIsReady() {
 
     $('.the-list-of-videos .loading').show();
 
