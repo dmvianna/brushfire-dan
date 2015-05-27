846b68dfe518738c3d780b64116727b6e5c10a70
Daniel Vianna <dvianna@bunnings.com.au>
2015-05-27 15:45:46 +0800

    AngularJS: video page without submit.

--- assets/js/videos-page.js
+++ assets/js/videos-page.js
@@ -1,63 +1,31 @@
-$(function whenDomIsReady() {
-    $('.the-submit-video-form').submit(function(e) {
-        e.preventDefault();
-
-        var newVideo = {
-            title: $('.the-submit-video-form input[name="title"]').val(),
-            src: $('.the-submit-video-form input[name="src"]').val()
-        };
-
-        $('.the-submit-video-form input').val('');
-        $('.the-submit-video-form button').text('submitting...');
-        $('.the-submit-video-form button').prop('disabled', true);
-
-        var parser = document.createElement('a');
-        parser.href = newVideo.src
-        var youtubeID = parser.search.substring(parser.search.indexOf("=") + 1,
-                                                parser.search.length);
-        newVideo.src = 'https://www.youtube.com/embed/' + youtubeID;
-
-        setTimeout(function(){
-            var newVideoHtml = '<li class="video">' +
-                '   <h2>' + newVideo.title + '</h2>' +
-                '   <iframe width="640" height="390" src="' + newVideo.src +
-                '" frameborder="0" allowfullscreen></iframe>' + '</li>';
-
-            $('.the-list-of-videos').prepend(newVideoHtml);
-            $('.the-submit-video-form button').text('Submit Video');
-            $('.the-submit-video-form button').prop('disabled', false);
-        }, 750);
+angular.module('brushfire_videosPage', [])
+    .config(function($sceDelegateProvider) {
+        $sceDelegateProvider.resourceUrlWhitelist([
+            'self',
+            '*://www.youtube.com/**'
+        ]);
     });
 
+angular.module('brushfire_videosPage').controller('PageCtrl', [
+    '$scope', '$timeout',
+    function ( $scope, $timeout ) {
 
-//$(function whenDomIsReady() {
+        $scope.videosLoading = true;
 
-    $('.the-list-of-videos .loading').show();
+        $timeout(function afterRetrievingVideos (){
+            var _videos = [{
+                title: 'Zombie Incidents',
+                src: 'https://www.youtube.com/embed/hD5SZ6v2JLQ'
+            }, {
+                title: 'Car crashes',
+                src: 'https://www.youtube.com/embed/e0LgMwmfmPU'
+            }, {
+                title: 'Forklift Incidents',
+                src: 'https://www.youtube.com/embed/EjOvI0TOx98'
+            }];
 
-    setTimeout(function afterRetrievingVideos() {
-        var videos = [{
-            title: 'Zombie incidents',
-            src: "https://www.youtube.com/embed/hD5SZ6v2JLQ"
-        }, {
-            title: 'Forklift incidents',
-            src: "https://www.youtube.com/embed/EjOvI0TOx98"
-        }, {
-            title: 'Truck incidents',
-            src: "https://www.youtube.com/embed/v8P_XuzgZac"
-        }];
+            $scope.videosLoading = false;
 
-        $('.the-list-of-videos .loading').hide();
-
-        var videosHtml = _.reduce(videos, function (html, video) {
-            html += '<li class="video">' +
-                ' <h2>' + video.title + '</h2>' +
-                ' <iframe width="640" height="390" src="' + video.src +
-                '" frameborder="0" allowfullscreen></iframe>' +
-                '</li>';
-            return html;
-        }, '');
-
-        $('.the-list-of-videos ul').replaceWith(videosHtml);
-
-    }, 750);
-});
+            $scope.videos = _videos;
+        }, 750);
+    }]);
