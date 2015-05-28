742ff89e27e89c995517f0869a7e5018f9bc6f48
Daniel Vianna <dvianna@bunnings.com.au>
2015-05-28 08:06:40 +0800

    Submit new video with Angular

--- assets/js/videos-page.js
+++ assets/js/videos-page.js
@@ -27,5 +27,37 @@ angular.module('brushfire_videosPage').controller('PageCtrl', [
             $scope.videosLoading = false;
 
             $scope.videos = _videos;
-        }, 750);
+
+            $scope.submitNewVideo = function() {
+
+                if ($scope.busySubmittingVideo) {
+                    return;
+                }
+
+                var _newVideo = {
+                    title: $scope.newVideoTitle,
+                    src: $scope.newVideoSrc,
+                };
+
+                var parser = document.createElement('a');
+
+                parser.href = _newVideo.src
+
+                var youtubeID = parser.search.substring(
+                    parser.search.indexOf("=") + 1,
+                    parser.search.length);
+
+                _newVideo.src = 'https://www.youtube.com/embed/' + youtubeID;
+
+                $scope.busySubmittingVideo = true;
+
+                $timeout(function() {
+                    $scope.videos.unshift(_newVideo);
+                    $scope.busySubmittingVideo = false;
+                    $scope.newVideoTitle = '';
+                    $scope.newVideoSrc = '';
+            
+                }, 750);
+            };
+        });
     }]);
